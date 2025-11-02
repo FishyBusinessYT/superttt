87c91afd0bdf9008a828ff132f704b3e5f4e2e3b
Cristóbal González <cristobalj.gonzalez@gmail.com>
2025-11-02 19:24:59 -0300

    Removed getvalidmoves from GameState

--- gameState.lua
+++ gameState.lua
@@ -1,7 +1,6 @@
 local BU = require('binUtils')
-local M = {}
 
-M.newGameState = function()
+return function()
     local self = {}
 
     self.xmarks = { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
@@ -12,10 +11,10 @@ M.newGameState = function()
     ---Check who's won this board
     ---@param board integer 1-9
     ---@return integer? 1 for X, 2 for O, nil for neither
-    self.isBoardTaken = function (board)
-        if BU.getBit(M.xwon, board) == 1 then
+    self.isBoardTaken = function(board)
+        if BU.getBit(self.xwon, board) == 1 then
             return 1
-        elseif BU.getBit(M.owon, board) == 1 then
+        elseif BU.getBit(self.owon, board) == 1 then
             return 2
         end
     end
@@ -24,49 +23,13 @@ M.newGameState = function()
     ---@param board integer 1-9
     ---@param cell integer 1-9
     ---@return integer? 1 for X, 2 for O, nil for neither
-    self.isCellTaken = function (board, cell)
-        if BU.getBit(M.xmarks[board], cell) == 1 then
+    self.isCellTaken = function(board, cell)
+        if BU.getBit(self.xmarks[board], cell) == 1 then
             return 1
-        elseif BU.getBit(M.omarks[board], cell) == 1 then
+        elseif BU.getBit(self.omarks[board], cell) == 1 then
             return 2
         end
     end
 
-    ---Add the empty cells from `board` into `moves`
-    ---@param board integer 1-9
-    ---@param moves table The current list of valid moves
-    local function addEmptyCells(board, moves)
-        for cell = 1, 9 do
-            if self.isCellTaken(board, cell) then goto nextCell end
-            table.insert(moves, { board, cell })
-
-            ::nextCell::
-        end
-    end
-
-    ---Get all valid moves from the current board position
-    ---@param lastPickedCell integer? 1-9
-    ---@return table moves List of valid moves
-    self.getLegalMoves = function(lastPickedCell)
-        local moves = {}
-
-        if lastPickedCell and ~self.isBoardTaken(lastPickedCell) then
-            addEmptyCells(lastPickedCell, moves)
-            return moves
-        end
-
-        for board = 1, 9 do
-            if self.isBoardTaken(board) then goto nextBoard end
-            addEmptyCells(board, moves)
-
-            ::nextBoard::
-        end
-
-        return moves
-    end
-
     return self
 end
-
-return M
-
