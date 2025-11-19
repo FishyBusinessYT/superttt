93e8c09b58b9c50064ef59fa99a975dea243dc3a
Cristóbal González <cristobalj.gonzalez@gmail.com>
2025-11-19 20:17:24 -0300

    Initial state, moving as X and making invalid moves as O, move undo.

--- test/gameState.lua
+++ test/gameState.lua
@@ -1,39 +1,43 @@
-local GS = require('classes.gameState')
+local gameState = require('gameState')()
 
-local gameState = GS()
+--- Test initial state
+local function testInitialState()
+    assert(gameState.isXsTurn)
+    assert(gameState.xwon == 0)
+    assert(gameState.owon == 0)
 
-local function printable(tbl, level)
-    level = level or 0
-    local indent = string.rep('\t', level)
-
-    io.write(indent .. '{\n')
-    for key, value in pairs(tbl) do
-        if type(value) == 'table' then
-            printable(value, level + 1)
-        else
-            io.write(indent .. string.format('[%s] = %s', key, value))
-        end
-        io.write(',\n')
-    end
-    io.write(indent .. '}\n')
-end
-
-for _ = 1, 4 do
-    local legalMoves = gameState.getLegalMoves()
-    local pickedMove = math.random(1, #legalMoves)
-    local move = legalMoves[pickedMove]
-
-    gameState.placeMark(move[1], move[2])
-
-    if math.random(1, 2) == 2 then
-        print(_)
-        printable(gameState)
-
-        gameState.undoMove()
-        gameState.placeMark(move[1], move[2])
-
-        printable(gameState)
+    for i = 1, 9 do
+        assert(gameState.xmarks[i] == 0)
+        assert(gameState.omarks[i] == 0)
     end
 end
+testInitialState()
 
-return gameState
+--- Test moves TODO TEST MOVING AS O AS WELL
+-- First mark can be placed anywhere
+assert(pcall(gameState.placeMark, { 1, 1 }))
+
+assert(gameState.xmarks[1] == 1)
+assert(not gameState.isXsTurn)
+
+-- Trying to place a mark on any board other than 5 should raise an exception
+-- and not change the game state
+for i = 2, 9 do
+    local succeeded = pcall(gameState.placeMark, { i, 1 })
+
+    assert(not succeeded)
+    assert(not gameState.isXsTurn)
+    assert(gameState.omarks[i] == 0)
+end
+
+--- Test move undo
+-- Logically, undoing the first ever move should restore the game to its
+-- original state.
+gameState.undoMove()
+testInitialState()
+
+-- Undoing another move should raise an exception and not change the game state
+-- at all.
+assert(not pcall(gameState.undoMove))
+testInitialState()
+
--- test/init.lua
+++ test/init.lua
@@ -1,2 +1,22 @@
 require('test.gameState')
 
+
+
+
+
+local function printable(tbl, level)
+    level = level or 0
+    local indent = string.rep('\t', level)
+
+    io.write(indent .. '{\n')
+    for key, value in pairs(tbl) do
+        if type(value) == 'table' then
+            printable(value, level + 1)
+        else
+            io.write(indent .. string.format('[%s] = %s', key, value))
+        end
+        io.write(',\n')
+    end
+    io.write(indent .. '}\n')
+end
+
