cdf1ca721bfe487db99e163f4bb2cda353058223
Cristóbal González <cristobalj.gonzalez@gmail.com>
2025-11-29 13:05:48 -0300

    Separated move tests and created a helper function to reduce code repetition

---
+++ test/gameState/moves.lua
@@ -0,0 +1,63 @@
+local gameState = require('gameState')()
+local ensure = require('test.ensure')
+
+-- This test assumes GameState.getCellOwner works properly
+print('Testing GameState.placeMark and GameState.undoMove')
+
+---One place to test placeMark under different circumstances
+---@param board integer
+---@param cell integer
+---@param isXMark boolean
+---@param shouldWork boolean
+local function testMove(board, cell, isXMark, shouldWork)
+    local cellPosStr = 'b' .. board .. 'c' .. cell
+    local playerStr = isXMark and 'X' or 'O'
+
+    -- This will end up looking something like:
+    -- 'Ensure placeMark call by X on cellPos b1c1 succeeds'
+    local ensureMsg = 'Ensure placeMark call by '
+        .. playerStr
+        .. ' on cellPos '
+        .. cellPosStr
+    ensureMsg = ensureMsg .. (shouldWork and ' succeeds' or ' fails')
+
+    -- Needs to be stored before calling placeMark. See the else block below.
+    local prevCellOwner = gameState.getCellOwner({ board, cell })
+
+    ensure(pcall(gameState.placeMark, { board, cell }), shouldWork, ensureMsg)
+
+    if shouldWork then
+        ensure(gameState.isXsTurn, not isXMark, 'Verify turn change')
+
+        ensure(
+            gameState.getCellOwner({ board, cell }),
+            isXMark and 1 or 2,
+            'Verify mark is saved'
+        )
+    else
+        ensure(gameState.isXsTurn, isXMark, 'Verify turn does not change')
+
+        -- The actual cell owner can be any of { 1, 2, nil } as long as that's
+        -- the value it held before calling placeMark.
+        ensure(
+            prevCellOwner,
+            gameState.getCellOwner({ board, cell }),
+            'Verify mark is not saved'
+        )
+    end
+end
+
+-- First mark (X's) can be placed anywhere
+testMove(1, 1, true, true)
+
+-- placeMark should disallow playing on any board other than b1,
+-- while preserving the game state
+for i = 2, 9 do
+    testMove(i, 1, false, false)
+end
+
+-- placeMark should disallow playing over an occupied cell as well
+testMove(1, 1, false, false)
+
+-- O should be able to place a mark on board 1
+testMove(1, 2, false, true)
