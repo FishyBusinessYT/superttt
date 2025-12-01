e9e04e5406f7b240b208d8c322973189b7d40068
Cristóbal González <cristobalj.gonzalez@gmail.com>
2025-12-01 16:14:08 -0300

    Win state checking tasks

--- test/gameState/winState.lua
+++ test/gameState/winState.lua
@@ -1,6 +1,2 @@
---- Test #3
-print('Testing GameState.getBoardOwner and win state checking')
-
-local gameState = require('gameState')()
-local ensure = require('test.ensure')
-
+--- CHECK GAME WIN STATE
+--- CHECK GAME DRAWS (HIGHLY UNLIKELY THOUGH)
