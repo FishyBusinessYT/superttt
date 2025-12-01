4e9ffef152547ea243c1ddbc3095067357b525f5
Cristóbal González <cristobalj.gonzalez@gmail.com>
2025-12-01 15:40:58 -0300

    Some board taking tests

---
+++ test/gameState/boardTaking.lua
@@ -0,0 +1,37 @@
+--- Test #3
+print('Testing board taking')
+
+local gameState = require('gameState')()
+local ensure = require('test.ensure')
+
+-- First let's have X take board 1
+gameState.placeMark({ 1, 2 }) -- X
+gameState.placeMark({ 2, 1 }) -- O
+gameState.placeMark({ 1, 3 }) -- X
+gameState.placeMark({ 3, 1 }) -- O
+gameState.placeMark({ 1, 1 }) -- X takes board 1
+
+ensure(
+    gameState.getBoardOwner(1),
+    1,
+    'Board checking marks board 1 as taken by X'
+)
+
+-- Undo every move
+for _ = 1, 5 do
+    gameState.undoMove()
+end
+
+--Now let's have O take board 2
+gameState.placeMark({4, 2}) -- X
+gameState.placeMark({2, 1}) -- O
+gameState.placeMark({1, 2}) -- X
+gameState.placeMark({2, 3}) -- O
+gameState.placeMark({3, 2}) -- X
+gameState.placeMark({2, 2}) -- O Takes board 2
+
+ensure(
+    gameState.getBoardOwner(2),
+    2,
+    'Board checking marks board 2 as taken by O'
+)
