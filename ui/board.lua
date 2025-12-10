675408b1dde828cf52e5f3a467dab41fe5c9356c
Cristóbal González <cristobalj.gonzalez@gmail.com>
2025-12-10 17:03:50 -0300

    It looks like it works; Shortest branch ever?

--- ui/board.lua
+++ ui/board.lua
@@ -17,17 +17,18 @@ return function()
         return { b, c }
     end
 
-    ---Draw this board to the screen
-    ---@param gameState GameState
-    self.draw = function(gameState)
-        love.graphics.setColor(0, 0, 0)
-        for y = 0, 8 do
+    local function drawGrid()
+        for i = 1, 8 do
             love.graphics.setLineWidth(1)
-            if y % 3 == 0 then love.graphics.setLineWidth(5) end
+            if i % 3 == 0 then love.graphics.setLineWidth(5) end
 
-            love.graphics.line(0, y * cell_size, 9 * cell_size, y * cell_size)
-            love.graphics.line(y * cell_size, 0, y * cell_size, 9 * cell_size)
+            love.graphics.line(0, i * cell_size, 9 * cell_size, i * cell_size)
+            love.graphics.line(i * cell_size, 0, i * cell_size, 9 * cell_size)
+        end
+    end
 
+    local function drawMarks(gameState)
+        for y = 0, 8 do
             for x = 0, 8 do
                 local owner = gameState.getCellOwner(XYtoCellPos(x + 1, y + 1))
 
@@ -38,6 +39,46 @@ return function()
                 end
             end
         end
+    end
+
+    ---Highlight the boards that have been taken by either player
+    ---@param gameState GameState
+    local function highlightTaken(gameState)
+        for i = 0, 8 do
+            local owner = gameState.getBoardOwner(i + 1)
+            local x = (i % 3) * cell_size * 3
+            local y = math.floor(i/3) * cell_size * 3
+            if owner == 1 then
+                love.graphics.setColor(0.5, 0, 0, 0.5)
+                love.graphics.rectangle(
+                    'fill',
+                    x,
+                    y,
+                    cell_size * 3,
+                    cell_size * 3
+                )
+            elseif owner == 2 then
+                love.graphics.setColor(0, 0, 0.5, 0.5)
+                love.graphics.rectangle(
+                    'fill',
+                    x,
+                    y,
+                    cell_size * 3,
+                    cell_size * 3
+                )
+            end
+        end
+    end
+
+    ---Draw this board to the screen
+    ---@param gameState GameState
+    self.draw = function(gameState)
+        love.graphics.setColor(0, 0, 0)
+
+        drawGrid()
+        drawMarks(gameState)
+        highlightTaken(gameState)
+        --highlightLegalMoves(gameState)
     end
 
     return self
