9bb6b704cd8f3e7f790a28baac114e2d0925f1f8
Cristóbal González <cristobalj.gonzalez@gmail.com>
2025-12-11 15:50:28 -0300

    It looks a little better now, utilizing the full screen width

--- ui/bar.lua
+++ ui/bar.lua
@@ -7,18 +7,29 @@ return function()
     local text = {
         xTurn = love.graphics.newText(font, 'Player X\'s turn'),
         oTurn = love.graphics.newText(font, 'Player O\'s turn'),
+        xWinner = love.graphics.newText(font, 'Player X has won!'),
+        oWinner = love.graphics.newText(font, 'Player O has won!'),
     }
     local color = {
         text = { 0, 0, 0 },
         bar = { 0.6, 0.7, 0.8 },
     }
 
-    self.draw = function(isXsTurn)
+    self.draw = function(isXsTurn, winner)
         love.graphics.setColor(color.bar)
-        love.graphics.rectangle('fill', 0, 900, 900, 100)
+        love.graphics.rectangle('fill', 0, 1100, WINWIDTH, WINHEIGHT)
 
         love.graphics.setColor(color.text)
-        love.graphics.draw(isXsTurn and text.xTurn or text.oTurn, 10, 935)
+        local currentText
+        if winner then
+            currentText = winner == 1 and text.xWinner or text.oWinner
+        else
+            currentText = isXsTurn and text.xTurn or text.oTurn
+        end
+
+        local textX = (WINWIDTH - currentText:getWidth()) / 2
+        local textY = WINHEIGHT - (100 - currentText:getHeight())
+        love.graphics.draw(currentText, textX, textY)
     end
     return self
 end
